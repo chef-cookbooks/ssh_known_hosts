d1a6cd566913e3cfddeead52084370261e7cf315
jtimberman <joshua@opscode.com>
2012-11-13 23:08:15 -0700

    add name attribute to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
-maintainer        "Scott M. Likens"
-maintainer_email  "scott@likens.us"
+name              "ssh_known_hosts"
+maintainer        "Opscode, Inc."
+maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Dyanmically generates /etc/ssh/known_hosts based on search indexes"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.5.0"
+version           "0.5.1"
 recipe            "ssh_known_hosts", "Dyanmically generates /etc/ssh/known_hosts based on search indexes"
