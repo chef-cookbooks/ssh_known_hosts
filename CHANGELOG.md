d830ebecc01dd29db6ccfba125f9bb4824515103
jtimberman <joshua@opscode.com>
2013-01-30 15:54:53 -0700

    release v0.6.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v0.6.0:
+
+* [COOK-2268] - Allow to run with chef-solo
+
 ## v0.5.0:
 
 * [COOK-1077] - allow adding arbitrary host keys from a data bag
--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Dyanmically generates /etc/ssh/known_hosts based on search indexes"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.5.1"
+version           "0.6.0"
 recipe            "ssh_known_hosts", "Dyanmically generates /etc/ssh/known_hosts based on search indexes"
