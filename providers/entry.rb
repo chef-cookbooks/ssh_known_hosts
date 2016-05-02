cadfb3f511d3bc288fc6eaeecc22034f2ac62878
Ben Weissmann <benweissmann@gmail.com>
2016-05-02 19:17:09 -0400

    Rubocop fix

--- providers/entry.rb
+++ providers/entry.rb
@@ -34,21 +34,21 @@ action :create do
                  new_resource.key_type
                end
 
-    if new_resource.port != 22
-      key = "[#{new_resource.host}]:#{new_resource.port} #{key_type} #{new_resource.key}"
-    else
-      key = "#{new_resource.host} #{key_type} #{new_resource.key}"
-    end
+    key = if new_resource.port != 22
+            "[#{new_resource.host}]:#{new_resource.port} #{key_type} #{new_resource.key}"
+          else
+            "#{new_resource.host} #{key_type} #{new_resource.key}"
+          end
 
   else
 
     keyscan_result = `ssh-keyscan -t#{node['ssh_known_hosts']['key_type']} -p #{new_resource.port} #{new_resource.host}`
 
-    if new_resource.port != 22
-      key = keyscan_result.sub(/^#{new_resource.host}/, "[#{new_resource.host}]:#{new_resource.port}")
-    else
-      key = keyscan_result
-    end
+    key = if new_resource.port != 22
+            keyscan_result.sub(/^#{new_resource.host}/, "[#{new_resource.host}]:#{new_resource.port}")
+          else
+            keyscan_result
+          end
 
   end
   comment = key.split("\n").first || ''
