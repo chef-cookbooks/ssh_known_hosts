de07629a77efcb3427630dae561811740a427508
Tim Smith <tsmith@chef.io>
2016-09-07 21:02:00 -0700

    Require chef 12
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,5 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '3.1.0'
 recipe            'ssh_known_hosts', 'Provides an LWRP for managing SSH known hosts. Also includes a recipe for automatically adding all nodes to the SSH known hosts.'
 
-source_url 'https://github.com/chef-cookbooks/ssh_known_hosts' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/ssh_known_hosts' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/ssh_known_hosts'
+issues_url 'https://github.com/chef-cookbooks/ssh_known_hosts'
