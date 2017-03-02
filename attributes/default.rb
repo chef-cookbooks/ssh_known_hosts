2226f695fc1edaec55e0e33f42c17787871cde80
Akos Vandra <akos@vandra.hu>
2017-03-02 13:23:29 +0100

    move cacher attributes back to default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -21,3 +21,6 @@ default['ssh_known_hosts']['file'] = '/etc/ssh/ssh_known_hosts'
 default['ssh_known_hosts']['key_types'] = %w(ed25519 ecdsa rsa dsa)
 default['ssh_known_hosts']['first_key_only'] = false
 
+default['ssh_known_hosts']['cacher']['data_bag'] = 'server_data'
+default['ssh_known_hosts']['cacher']['data_bag_item'] = 'known_hosts'
+
