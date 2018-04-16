b41054001c6040f12d9734937b27ff3e1f705985
Tim Smith <tsmith@chef.io>
2018-04-16 10:13:37 -0700

    Add 2 hosts in the test recipe
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/ssh_known_hosts_test/recipes/resource.rb
+++ test/fixtures/cookbooks/ssh_known_hosts_test/recipes/resource.rb
@@ -1,5 +1,3 @@
-apt_update 'update'
-
 include_recipe 'openssh::default'
 
 group 'knownhosts' do
@@ -11,6 +9,12 @@ user 'knownhosts' do
   comment 'Testing user'
   uid '9000'
   gid '9000'
+end
+
+ssh_known_hosts_entry 'travis.org' do
+  owner 'knownhosts'
+  group 'knownhosts'
+  mode '0600'
 end
 
 # don't do this in production as ssh_known_hosts needs to be world readable
